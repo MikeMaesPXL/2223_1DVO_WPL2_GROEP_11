0ef8c64ba9f7fe63b7b1c2fec52a41dddc4c27ce
Four <105184275+MikeMWD05@users.noreply.github.com>
2023-05-13 17:52:42 +0200

    Winkelmandje werkend gekregen, maar zitten nog bugs in -> cross scherm maken

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ if (document.readyState == 'loading') {
 
 function ready() {
     let removeCartItemsButtons = document.getElementsByClassName('btn-danger')
-    console.log(removeCartItemsButtons)
     for (let i = 0; i < removeCartItemsButtons.length; i++) {
         let button = removeCartItemsButtons[i]
         button.addEventListener("click", removeCartitem)
@@ -16,6 +15,12 @@ function ready() {
     for (let i = 0; i < quanityInputs.length; i++) {
         let input = quanityInputs[i]
         input.addEventListener('change', quantityChanged)
+    }
+
+    let addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (let i = 0; i < addToCartButtons.length; i++) {
+        let button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
     }
 }
 
@@ -33,6 +38,46 @@ function quantityChanged(e) {
     updateCartTotal()
 }
 
+function addToCartClicked(e) {
+    let button = e.target
+    let shopItem = button.parentElement.parentElement.parentElement
+    let title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    let price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    let imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    let cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    let cartItems = document.getElementsByClassName('cart-items')[0]
+    let cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (let i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('Dit product is al in je winkelmandje.')
+            return
+        }
+    }
+    let cartRowContents = `
+            <div class="cart-item cart-column">
+                <img class="cart-item-image" src="${imageSrc}" alt="img" width="75" height="75">
+                <span class="cart-item-title">${title}</span>
+            </div>
+            <span class="cart-price cart-column">${price}</span>
+            <div class="cart-quantity cart-column">
+                <input class="cart-quantity-input" type="number" value="1">
+                <button class="btn btn-danger" type="button">REMOVE</button>
+            </div>
+    `
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click',
+    removeCartitem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change',
+    quantityChanged)
+}
+
 function updateCartTotal() {
     let cartItemContainer = document.getElementsByClassName('cart-items')[0]
     let cartRows = cartItemContainer.getElementsByClassName('cart-row')
@@ -45,5 +90,6 @@ function updateCartTotal() {
         let quantity = quantityElement.value
         total = total + (price * quantity)
      }
+     total = Math.round(total * 100) / 100
      document.getElementsByClassName('cart-total-price')[0].innerText = '€' + total
 }
