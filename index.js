907ef3fbd1ec3948bae584782dc9e0c7608411ae
Emile Melnikov <emilemelnikov@Emiles-MacBook-Pro.local>
2023-05-23 11:01:03 +0200

    Website aangepast, minor changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let currentPhone = 0;
+let phoneImages = ["phoneImg", "phoneImg2", "phoneImg3"];
+
 function selectPhone(selectedPhone) {
     let phone1 = document.getElementById("phoneImg");
     let phone2 = document.getElementById("phoneImg2");
@@ -9,10 +12,23 @@ function selectPhone(selectedPhone) {
 
     if (selectedPhone === 1) {
         phone1.classList.remove("phone-image-grayed-out");
+        currentPhone = 0;
     } else if (selectedPhone === 2) {
         phone2.classList.remove("phone-image-grayed-out");
+        currentPhone = 1;
     } else if (selectedPhone === 3) {
         phone3.classList.remove("phone-image-grayed-out");
+        currentPhone = 2;
     }
 }
 
+function changePhoneImage() {
+    if (currentPhone + 1 >= phoneImages.length) {
+        currentPhone = 0;
+    } else {
+        currentPhone = currentPhone + 1;
+    }
+    selectPhone(currentPhone + 1);
+}
+
+setInterval(changePhoneImage, 4000);
